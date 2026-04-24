0804e149055b1961165eda6c56caee295025c9b9
Pablo Marelli <pablo.marelli@whatifmediagroup.com>
2026-04-24 20:29:45 -0300

    Oil.nvim git status.

--- private_dot_config/nvim/lua/plugins/oil.lua
+++ private_dot_config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,17 @@
 return {
   'stevearc/oil.nvim',
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  dependencies = {
+    { "nvim-mini/mini.icons", opts = {} },
+    "SirZenith/oil-vcs-status",
+  },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
---
+  --
   opts = {
+    win_options = {
+      number = false,
+      relativenumber = false,
+      signcolumn = "yes:1",
+    },
     view_options = {
       show_hidden = true,
     },
@@ -13,6 +21,26 @@ return {
       ["<C-l>"] = false, -- Disable refresh (conflicts with tab navigation)
     },
   },
+  config = function(_, opts)
+    require("oil").setup(opts)
+    local StatusType = require("oil-vcs-status.constant.status").StatusType
+
+    require("oil-vcs-status").setup({
+      status_symbol = {
+        [StatusType.Added] = "+",
+        [StatusType.Copied] = "C",
+        [StatusType.Deleted] = "-",
+        [StatusType.Ignored] = "!",
+        [StatusType.Modified] = "~",
+        [StatusType.Renamed] = ">",
+        [StatusType.TypeChanged] = "T",
+        [StatusType.Unmodified] = " ",
+        [StatusType.Unmerged] = "U",
+        [StatusType.Untracked] = "?",
+        [StatusType.External] = "X",
+      },
+    })
+  end,
   keys = {
     {
       "<leader>e",
